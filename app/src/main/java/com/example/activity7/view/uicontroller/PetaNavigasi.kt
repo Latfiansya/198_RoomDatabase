0007456e163c41e59a6d189735768b2750f2295c
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-28 13:50:30 +0700

    Membuat PetaNavigasi.kt dan AppBar.kt

--- app/src/main/java/com/example/activity7/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity7/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,44 @@
 package com.example.activity7.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activity7.view.HalamanEntry
+import com.example.activity7.view.HomeScreen
+import com.example.activity7.view.route.DestinasiHome
+import com.example.activity7.view.route.DestinasiEntry
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(),
+             modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HalamanHome(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            HalamanEntry(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
