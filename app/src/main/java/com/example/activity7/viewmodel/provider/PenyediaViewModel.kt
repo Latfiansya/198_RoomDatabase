0715465bbb0627d13819aa2f7e0598faf9e2fc10
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-28 13:26:22 +0700

    Menambahkan penyedia untuk VM

--- app/src/main/java/com/example/activity7/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/activity7/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.activity7.viewmodel.provider
 
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.activity7.repositori.AplikasiSiswa
+import com.example.activity7.viewmodel.EntryViewModel
+import com.example.activity7.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
