7207bf1d7f90f8fe035de0699cc49445ede64b91
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-28 13:13:01 +0700

    Menamakan objek DestinasiEntry.kt

--- app/src/main/java/com/example/activity7/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/activity7/view/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.activity7.view.route
 
-object DestinasiEntry {
+import com.example.activity7.R
+import com.example.activity7.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
